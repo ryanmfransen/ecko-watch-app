64889ddc55831052a32b64fda475e4f66032b3ce
Ryan Fransen <ryan.fransen@me.com>
2025-12-26 09:38:36 -0800

    Initial Commit

---
+++ ecko Watch App/eckoApp.swift
@@ -0,0 +1,17 @@
+//
+//  eckoApp.swift
+//  ecko Watch App
+//
+//  Created by Ryan Fransen on 2025-12-26.
+//
+
+import SwiftUI
+
+@main
+struct ecko_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
